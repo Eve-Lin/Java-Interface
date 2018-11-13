af210fbb8dadb3bb809cb1761d9f2a2057a1124e
Eve Lin <34218569+Eve-Lin@users.noreply.github.com>
2018-11-13 18:09:04 +0200

    Multiple Interfaces

---
+++ MyClass.java
@@ -0,0 +1,31 @@
+interface FirstInterface {
+
+    public void myMethod();
+}
+
+interface SecondInterface {
+
+    public void myOtherMethod();
+}
+
+
+class ImplementingClass implements FirstInterface, SecondInterface{
+
+    @Override
+    public void myMethod() {
+        System.out.println("Hi there!");
+    }
+
+    @Override
+    public void myOtherMethod() {
+        System.out.println("Nice to meet you!");
+    }
+}
+public class MyClass {
+    public static void main(String[] args) {
+
+        ImplementingClass myObj = new ImplementingClass();
+        myObj.myMethod();
+        myObj.myOtherMethod();
+    }
+}
