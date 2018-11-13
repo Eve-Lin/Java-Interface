c134ec59bf582380bda6cd336dd62d5411a930a1
Eve Lin <evelin.dicheva@gmail.com>
2018-11-13 13:18:01 +0200

    Java Interface Example

---
+++ Animal.java
@@ -0,0 +1,26 @@
+interface Animal1 {
+    public void animalSound();
+    public void sleep();
+}
+
+
+
+class Pig implements Animal1{
+    public void animalSound(){
+        System.out.println("The pig says: wee wee");
+    }
+    public void sleep (){
+        System.out.println("Zzz");
+    }
+}
+
+public class Animal {
+
+    public static void main(String[] args) {
+        Pig piggy = new Pig();
+        piggy.animalSound();
+        piggy.sleep();
+    }
+
+}
+
